252bec3385549b7930d460bce94ef091b2d661d8
Ajit <d.ajitchandran@icloud.com>
2022-11-19 17:01:43 +0000

    modified ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,7 +1,5 @@
 provider "aws" {
     region = "eu-west-2"
-    access_key = "AKIA2SV4SDECVVUW274T"
-    secret_key = "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"
 }
 
 resource "aws_instance" "ec2" {
